71a0628e75fe996dedfa66f27e17d8511be67953
Ben <benknight135@gmail.com>
2021-12-11 23:11:34 +0000

    add mysql database

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+    echo "<h1>Hello world!</h1>";
 
-echo "Hello World!";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    echo "<h2>Connected successfully</h2>";
+?>
