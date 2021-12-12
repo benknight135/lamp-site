dcdc843338867c0dccd8bb01461b337c48254da5
Ben <benknight135@gmail.com>
2021-12-12 16:48:41 +0000

    add no cache

--- web/index.php
+++ web/index.php
@@ -5,6 +5,11 @@ require_once('../vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
+header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+
 // Create welcome message
 $hello = new LampSite\Hello();
 $hello_msg = $hello->message();
