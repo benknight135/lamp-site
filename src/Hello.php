c273fa84ab1bfbf1ac56bb40a543371e1d518dc3
Ben <benknight135@gmail.com>
2021-12-12 00:25:03 +0000

    add test and better project structure

---
+++ src/Hello.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace LampSite;
+
+class Hello
+{
+    /**
+     * Get hello message
+     * @return string Message
+     */
+    public function message()
+    {
+        return "hello world!";
+    }
+}
