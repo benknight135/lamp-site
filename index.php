07563222e464a0914b74539b6fefe019469e51d8
Ben <benknight135@gmail.com>
2021-12-12 17:42:31 +0000

    move index to root

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-require_once('../vendor/autoload.php');
+require_once('vendor/autoload.php');
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
