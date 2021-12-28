72b2d95490b1c7aa617facfc53355d8893dcefbb
Ben <benknight135@gmail.com>
2021-12-28 16:40:24 +0000

    improve env handelling

--- src/Database.php
+++ src/Database.php
@@ -12,8 +12,16 @@ class Database
     function __construct(){
         // load environment varibles
         $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
-        $dotenv->safeLoad();
-        $dotenv->required("APP_DEBUG");
+        try{
+            $dotenv->load();
+            $dotenv->required("APP_DEBUG");
+        } catch (\Dotenv\Exception\InvalidPathException $e){
+            // .env is missing when in production environment
+            error_log("Missing .env file. Assuming production environment.", 0);
+        }
+        if (!array_key_exists('APP_DEBUG', $_ENV)){
+            throw new \Exception("Missing APP_DEBUG environment variable");
+        }
         if ($_ENV['APP_DEBUG'] == true){
             // load database variable from .env
             $dotenv->required([
@@ -26,7 +34,9 @@ class Database
             $this->db_name = $_ENV['DB_DATABASE'];
         } else {
             // parse database varaibles from cleardb url
-            $dotenv->required("CLEARDB_DATABASE_URL");
+            if (!array_key_exists('CLEARDB_DATABASE_URL', $_ENV)){
+                throw new \Exception("Missing CLEARDB_DATABASE_URL environment variable");
+            }
             $url = parse_url($_ENV["CLEARDB_DATABASE_URL"]);
             $this->host = $url["host"];
             $this->username = $url["user"];
@@ -39,9 +49,10 @@ class Database
         // connect to database
         $this->conn = new \mysqli($this->host, $this->username, $this->password, $this->db_name);
         if ($this->conn->connect_errno) {
-            error_log("Failed to connect to database: " . $this->conn->connect_error, 0);
+            error_log("Database connection failed: " . $this->conn->connect_error, 0);
             return false;
         } else {
+            error_log("Database connection success", 0);
             return true;
         }
     }
