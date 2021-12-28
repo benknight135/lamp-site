f8b0a1fd547605a9db75d69ebd2b00606b8f398f
Ben <benknight135@gmail.com>
2021-12-28 22:39:14 +0000

    add phpunit for loading .env

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,8 +1,5 @@
 <?php
 
-declare(strict_types=1);
-require_once('vendor/autoload.php');
-
 use LampSite\Database;
 use PHPUnit\Framework\TestCase;
 
@@ -17,8 +14,6 @@ class DatabaseTest extends TestCase
         // TODO: find better way to load this file
         // this is needed as .env file loading doesn't work in
         // github actions???? 
-        $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
-        $dotenv->load();
         error_log( print_r($_ENV, TRUE) );
         $db_host = $_ENV["DB_HOST"];
         $db_user = $_ENV["DB_USERNAME"];
